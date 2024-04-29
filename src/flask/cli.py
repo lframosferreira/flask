c45af2e33a66940dbe8da2584c74c250dddb5dd6
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-04-29 16:13:40 -0300

    teste de software: aula pratica 4

--- src/flask/cli.py
+++ src/flask/cli.py
@@ -55,7 +55,8 @@ def find_best_app(module: ModuleType) -> Flask:
     matches = [v for v in module.__dict__.values() if isinstance(v, Flask)]
 
     if len(matches) == 1:
-        return matches[0]
+        # return matches[0]
+        return None
     elif len(matches) > 1:
         raise NoAppException(
             "Detected multiple Flask applications in module"
